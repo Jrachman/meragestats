448ecea6277c190749476947ad17f2a865a01550
Julian Rachman <jmrachman@gmail.com>
2019-06-08 12:09:07 -0700

    modularized codebase

--- parser.py
+++ parser.py
@@ -2,51 +2,62 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-user_email = "put email here"
-user_password = "put password here"
+USER_EMAIL = "katiexiong1998@hotmail.com"
+USER_PASSWORD = "PaulMerage123"
+LINKEDIN_ROOT_URL = "https://www.linkedin.com"
 
-# specifies the path to the chromedriver.exe
-driver = webdriver.Chrome("./chromedriver")
 
-# driver.get method() will navigate to a page given by the URL address
-driver.get("https://www.linkedin.com/uas/login?")
+def initialize_driver():
+    # specifies the path to the chromedriver.exe
+    return webdriver.Chrome("./chromedriver")
 
-# locate email form by_class_name
-username = driver.find_element_by_id("username")
 
-# send_keys() to simulate key strokes
-username.send_keys(user_email)
+def login_linkedin(driver):
+    # driver.get method() will navigate to a page given by the URL address
+    driver.get("https://www.linkedin.com/uas/login?")
 
-# locate password form by_class_name
-password = driver.find_element_by_id("password")
+    # locate email form by_class_name
+    username = driver.find_element_by_id("username")
+    # send_keys() to simulate key strokes
+    username.send_keys(USER_EMAIL)
 
-# send_keys() to simulate key strokes
-password.send_keys(user_password)
+    # locate password form by_class_name
+    password = driver.find_element_by_id("password")
+    # send_keys() to simulate key strokes
+    password.send_keys(USER_PASSWORD)
 
-# locate submit button by_class_name
-log_in_button = driver.find_element_by_xpath("//*[@id='app__container']/main/div/form/div[3]/button")
+    # locate submit button by_class_name
+    log_in_button = driver.find_element_by_xpath("//*[@id='app__container']/main/div/form/div[3]/button")
+    # locate submit button by_class_id
+    # log_in_button = driver.find_element_by_class_id('login submit-button')
+    # locate submit button by_xpath
+    # log_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
+    # .click() to mimic button click
+    log_in_button.click()
 
-# locate submit button by_class_id
-# log_in_button = driver.find_element_by_class_id('login submit-button')
 
-# locate submit button by_xpath
-# log_in_button = driver.find_element_by_xpath('//*[@type="submit"]')
+def get_profile_urls(driver):
+    merage_search_url = f"{LINKEDIN_ROOT_URL}/search/results/people/?facetSchool=%5B%2217948%22%5D&origin=FACETED_SEARCH&page="
 
-# .click() to mimic button click
-log_in_button.click()
-merage_search_url = "https://www.linkedin.com/search/results/people/?facetSchool=%5B%2217948%22%5D&origin=FACETED_SEARCH&page="
+    set_of_profiles = set()
+    for page_num in range(1, 101):
+        driver.get(f"{merage_search_url}{page_num}")
 
-set_of_profiles = set()
-for page_num in range(1, 101):
-    driver.get(f"{merage_search_url}{page_num}")
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        for link in soup.find_all("a"):
+            profile_url = link.get("href")
+            if "/in/" in profile_url and profile_url not in set_of_profiles:
+                print(profile_url)
+                set_of_profiles.add(profile_url)
+                # instead of adding to a set, run the linkedin profile extracter
+                # use linkedin username as unique identifier to eliminate the possibility of not being able to differentiate between two John Apple's
 
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    for link in soup.find_all("a"):
-        profile_url = link.get("href")
-        if "/in/" in profile_url and profile_url not in set_of_profiles:
-            print(profile_url)
-            set_of_profiles.add(profile_url)
-            # instead of adding to a set, run the linkedin profile extracter
+    print(set_of_profiles, len(set_of_profiles))
+    # maybe push information to csv
+    return set_of_profiles
 
-print(set_of_profiles, len(set_of_profiles))
-# maybe push information to csv
+
+if __name__ == "__main__":
+    driver = initialize_driver()
+    login_linkedin(driver)
+    get_profile_urls(driver)
