c6281d63a742dad452f9e824082e00e859f03dc1
Julian Rachman <jmrachman@gmail.com>
2019-06-09 12:50:19 -0700

    added auto scrolling and a new method of getting user profiles

--- parser.py
+++ parser.py
@@ -2,9 +2,10 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import string
+import time
 
-USER_EMAIL = "blank"
-USER_PASSWORD = "blank"
+USER_EMAIL = "paulmeragebusiness@gmail.com"
+USER_PASSWORD = "paulmerageismydad69"
 LINKEDIN_ROOT_URL = "https://www.linkedin.com"
 PEOPLE_SEARCH_URL = f"{LINKEDIN_ROOT_URL}/search/results/people/?"
 
@@ -41,23 +42,26 @@ def login_linkedin(driver):
 def get_profile_urls(driver):
     set_of_profiles = set()
 
-    for letter in string.ascii_lowercase:
-        search_args = f'facetNetwork=%5B"F"%2C"S"%2C"O"%5D&facetSchool=%5B%2217948%22%5D&keywords={letter}&origin=FACETED_SEARCH&page='
+    for connection_level in ["F", "S", "O"]:
+        for letter in string.ascii_lowercase:
+            search_args = f'facetNetwork=%5B"{connection_level}"%5D&facetSchool=%5B%2217948%22%5D&keywords={letter}&origin=FACETED_SEARCH&page='
 
-        for page_num in range(1, 101):
-            driver.get(f"{PEOPLE_SEARCH_URL}{search_args}{page_num}")
+            for page_num in range(1, 101):
+                driver.get(f"{PEOPLE_SEARCH_URL}{search_args}{page_num}")
 
-            soup = BeautifulSoup(driver.page_source, "html.parser")
-            for link in soup.find_all("a"):
-                profile_url = link.get("href")
-                if profile_url and "/in/" in profile_url and profile_url not in set_of_profiles:
-                    print(profile_url)
-                    set_of_profiles.add(profile_url)
-                    # instead of adding to a set, run the linkedin profile extracter
-                    # use linkedin username as unique identifier to eliminate the possibility of not being able to differentiate between two John Apple's
-            print(f"current number of profiles captured: {len(set_of_profiles)}, CURRENT letter: {letter}")
+                soup = BeautifulSoup(driver.page_source, "html.parser")
+                subset_of_profiles = {link.get("href") for link in soup.find_all("a") if link.get("href") and "/in/" in link.get("href") and link.get("href") not in set_of_profiles}
+                if not subset_of_profiles:
+                    break
+                else:
+                    set_of_profiles |= subset_of_profiles
+                # instead of adding to a set, run the linkedin profile extracter
+                # use linkedin username as unique identifier to eliminate the possibility of not being able to differentiate between two John Apple's
+                print(f"current number of profiles captured: {len(set_of_profiles)}, CURRENT letter: {letter}")
 
-        print(f"current number of profiles captured: {len(set_of_profiles)}, NEXT letter: {letter}")
+            print(f"current number of profiles captured: {len(set_of_profiles)}, CURRENT letter (completed): {letter}")
+
+        print(f"current number of profiles captured: {len(set_of_profiles)}, CURRENT connection level (completed): {connection_level}")
 
     print(set_of_profiles, len(set_of_profiles))
     # maybe push information to csv
@@ -65,7 +69,37 @@ def get_profile_urls(driver):
     return set_of_profiles
 
 
+def get_profile_urls_merage_page(driver):
+    set_of_profiles = set()
+    years = range(2008, 2020)
+
+    prefix_url = "https://www.linkedin.com/school/uci-paul-merage-school-of-business/people/"
+
+    for year in years:
+        year_args = f"?educationEndYear={year}&educationStartYear={year}"
+        driver.get(f"{prefix_url}{year_args}")
+
+        SCROLL_PAUSE_TIME = 0.5
+
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+
 if __name__ == "__main__":
     driver = initialize_driver()
     login_linkedin(driver)
-    set_of_profiles = get_profile_urls(driver)
+    # set_of_profiles = get_profile_urls(driver)
+    get_profile_urls_merage_page(driver)
